ee3559c5a53f6aeff66374c38e2d4ed564e2a78c
Zutoki <kevinhumberto9@hotmail.com>
2019-03-14 22:57:59 -0600

    Example 1 - Hello World
    This example is a simple Hello World.

---
+++ Example 1 - Hello World/main.c
@@ -0,0 +1,8 @@
+# include<stdio.h> // Printf, Scanf, etc.
+# include<stdlib.h> // 
+
+int main(){
+	printf("Hello World"); // Print a simple text between " double.
+	//system("pause"); // Pause the program. In version of Dev 5.11 not is necessary 
+	return 0; // return the program to zero resources or RAM.
+}
